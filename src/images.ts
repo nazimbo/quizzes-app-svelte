33c8417408126a46b4a98e10180ad32a9ba5c740
nazimbo <113207575+nazimbo@users.noreply.github.com>
2024-02-19 19:30:49 +0100

    Add quizzes to page

---
+++ src/images.ts
@@ -0,0 +1,11 @@
+export const chemistry = "https://images.theconversation.com/files/121885/original/image-20160510-20731-1pf8nwv.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1356&h=668&fit=crop";
+
+export const biology = "https://www.bolton.ac.uk/assets/What-is-Medical-Biology-Bolton-Uni__ResizedImageWzYwMCwzMzhd.jpg";
+
+export const mathematics = "https://s26162.pcdn.co/wp-content/uploads/2020/08/mathematics-symbols.jpg";
+
+export const history = "https://www.wlc.edu/_images/academics/IMPACT-History.jpg";
+
+export const physics = "https://0utwqfl7.cdn.imgeng.in/explore-academics/programs/images/undergraduate/henson/physicsmajorMH.jpg";
+
+export const geography = "https://educationblog.oup.com/wp-content/uploads/2015/09/oup_58381-705x435.jpg";
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
+  import quizzes from "../data.json";
+  import * as images from "../images";
 </script>
 
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
-
-<style lang="postcss">
-  :global(html) {
-    background-color: theme(colors.gray.100);
-  }
-</style>
+<div class="quizzes-container max-w-[800px] m-auto flex justify-center h-screen items-center flex-wrap">
+  {#each quizzes as quizz (quizz.id)}
+    <div class="quizz-card shadow w-[300px] rounded overflow-hidden cursor-pointer m-2">
+      <img class="h-44" src={images[quizz.imageUrl]} alt="" />
+      <div class="flex items-center justify-between px-3 py-2">
+        <h1 class="font-bold">{quizz.name}</h1>
+        <p>{quizz.questions.length} questions</p>
+      </div>
+    </div>
+  {/each}
+</div>
