d20cd5496cf2926fcda212fcd3bf6bfb1459e935
nazimbo <113207575+nazimbo@users.noreply.github.com>
2024-02-19 20:43:22 +0100

    Add new quiz card component and update data.json

---
+++ src/components/QuizzCard.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import * as images from "../images";
+
+  interface Quizz {
+    id: number;
+    name: string;
+    imageUrl: keyof typeof images;
+    questions: {
+      id: number;
+      text: string;
+      answer: string;
+      options: {
+        id: number;
+        text: string;
+        label: string;
+      }[];
+    }[];
+  }
+
+  export let quizz: Quizz;
+</script>
+
+<div class="quizz-card shadow w-[300px] rounded overflow-hidden cursor-pointer m-2">
+  <img class="h-44" src={images[quizz.imageUrl]} alt="" />
+  <div class="flex items-center justify-between px-3 py-2">
+    <h1 class="font-bold">{quizz.name}</h1>
+    <p>{quizz.questions.length} questions</p>
+  </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,10 @@
 <script lang="ts">
   import quizzes from "../data.json";
-  import * as images from "../images";
+  import QuizzCard from "../components/QuizzCard.svelte";
 </script>
 
 <div class="quizzes-container max-w-[800px] m-auto flex justify-center h-screen items-center flex-wrap">
   {#each quizzes as quizz (quizz.id)}
-    <div class="quizz-card shadow w-[300px] rounded overflow-hidden cursor-pointer m-2">
-      <img class="h-44" src={images[quizz.imageUrl]} alt="" />
-      <div class="flex items-center justify-between px-3 py-2">
-        <h1 class="font-bold">{quizz.name}</h1>
-        <p>{quizz.questions.length} questions</p>
-      </div>
-    </div>
+    <QuizzCard {quizz} />
   {/each}
 </div>
