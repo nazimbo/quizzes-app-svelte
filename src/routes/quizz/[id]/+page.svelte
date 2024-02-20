00da84ca4b2bd4040e836ebe40b69edb1f68b472
nazimbo <113207575+nazimbo@users.noreply.github.com>
2024-02-20 11:52:40 +0100

    Add QuestionButton component and subscribe to answers store

--- src/routes/quizz/[id]/+page.svelte
+++ src/routes/quizz/[id]/+page.svelte
@@ -1,19 +1,26 @@
 <script lang="ts">
   import QuestionText from "./components/QuestionText.svelte";
   import QuestionOption from "./components/QuestionOption.svelte";
+  import QuestionButton from "./components/QuestionButton.svelte";
+  import { answers, type Answer } from "../../../store";
 
   export let data: any;
 
   let currentQuestionIndex = 0;
+  let answersValue: Answer[];
+  answers.subscribe((value) => (answersValue = value));
+
   $: currentQuestion = data.questions[currentQuestionIndex];
 </script>
 
 <div class="w-full">
   <QuestionText question={currentQuestion.question} />
-  <!-- Options -->
+
   <div class="flex flex-wrap justify-between">
     {#each currentQuestion.options as option (option.id)}
       <QuestionOption {option} />
     {/each}
   </div>
+
+  <QuestionButton />
 </div>
---
+++ src/routes/quizz/[id]/components/QuestionButton.svelte
@@ -0,0 +1,3 @@
+<div class="text-center">
+  <button class="bg-blue-500 text-white font-bold p-4 px-16 rounded"> Submit </button>
+</div>
